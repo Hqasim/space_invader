52b10f5f28648a03dd1651a100ac8f0394296281
Hamzah Qasim <hqqasim55@gmail.com>
2020-11-15 12:51:17 +0500

    Added testing suit stub tests

---
+++ Tests/test_game.py
@@ -0,0 +1,34 @@
+# Test suit for game application
+import unittest
+
+
+class TestGame(unittest.TestCase):
+
+    def test_launch_application(self):
+        self.fail()
+
+    def test_play_game(self):
+        self.fail()
+
+    def test_pause_game(self):
+        self.fail()
+
+    def test_leaderboards(self):
+        self.fail()
+
+    def test_score_screen(self):
+        self.fail()
+
+    def test_game_terminate(self):
+        self.fail()
+
+    def test_ship_move_right(self):
+        self.fail()
+
+    def test_ship_move_left(self):
+        self.fail()
+
+
+if __name__ == '__main__':
+    unittest.main()
+
